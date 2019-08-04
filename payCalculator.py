a7ab2513469808b4b61dac0c43bac6d6ca084f4e
SabraMGrace <sabramgrace@gmail.com>
2019-08-04 00:59:02 -0700

    Add files via upload

---
+++ payCalculator.py
@@ -0,0 +1,34 @@
+# Sabra Grace
+# itc110
+# 8/4/19
+# Week 8 Assignment
+# payCalculator.py
+
+import math
+
+def main():
+    print("Weekly Pay Calculator")
+    payCalculator()
+
+# get input
+def payCalculator():
+    hours = float(input("How many hours did you work last week? "))
+    wage = float(input("What is your hourly wage? "))
+
+# account for overtime pay
+    if hours > 40:
+        regPay = 40 * wage
+        overtimeHours = hours - 40
+        overtimePay = overtimeHours * (wage * 0.5)
+        totalPay = overtimePay + regPay
+        print("You earned a total of $", + (totalPay))
+    elif hours <= 0:
+        print("You didn't work last week. You haven't earned any wages.")
+    elif hours > 168:
+        print("Are you kidding? You worked every single hour of the day last week? You need to go home and rest!")
+    else:
+        totalPay = hours * wage
+        print("You earned a total of $", + (totalPay))
+
+
+main()
